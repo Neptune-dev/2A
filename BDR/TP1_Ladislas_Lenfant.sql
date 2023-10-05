52964ab32e431af456680a626a50b888ec766682
Neptune <felix@ladislas.fr>
2023-10-05 21:45:41 +0200

    Update TP1_Ladislas_Lenfant.sql

--- BDR/TP1_Ladislas_Lenfant.sql
+++ BDR/TP1_Ladislas_Lenfant.sql
@@ -1,7 +1,7 @@
 --3
-SHOW DATABASES
-CREATE DATABASE TP1
-USE TP1
+SHOW DATABASES;
+CREATE DATABASE TP1;
+USE TP1;
 
 --4
 CREATE TABLE `Personne` ( 
@@ -14,8 +14,8 @@ CREATE TABLE `Personne` (
 );
 
 --5
-SHOW TABLES
-DESCRIBE Personne
+SHOW TABLES;
+DESCRIBE Personne;
 
 --6
 INSERT INTO Personne (prenom, nom, profession, date_naissance) 
@@ -51,6 +51,7 @@ CREATE TABLE `Est_Invite` (
 INSERT INTO Est_Invite 
 VALUES 
 (3, '2020-12-24','Champs-Elysées, Paris');
+-- l'index 3 n'existe pas dans la table Personne
 */ 
 INSERT INTO Personne (prenom, nom, profession, date_naissance) 
 VALUES 
@@ -58,7 +59,17 @@ VALUES
 
 --11
 
--- TO DO : ajouter des personnes
+INSERT INTO Personne (prenom, nom, profession, date_naissance) 
+VALUES 
+('Maxime', 'Lenfant', 'Boulanger', '2004-01-03');
+
+INSERT INTO Personne (prenom, nom, profession, date_naissance) 
+VALUES 
+('Félix', 'Ladislas', 'Enquêteur', '2004-04-14');
+
+INSERT INTO Personne (prenom, nom, profession, date_naissance) 
+VALUES 
+('Maurice', 'Ramenot', 'Psychologue', '1975-08-16');
 
 INSERT INTO Est_Invite 
 VALUES 
@@ -67,11 +78,119 @@ VALUES
 --12
 /*
 DROP TABLE;
+-- Une autre table dépend de celle-ci donc elle ne peut être supprimée
 */
 
 --13
 --a)
 SELECT * FROM Personne;
 --b)
-SELECT nom, prenom FROM Personne WHERE date_naissance > `1993-09-28`;
+SELECT nom, prenom FROM Personne WHERE date_naissance < '1993-09-28';
 --c)
+SELECT nom, prenom
+FROM Personne
+INNER JOIN Est_Invite
+ON Personne.id = Est_Invite.id;
+
+--14
+CREATE TABLE `Plat` ( 
+    `nom` VARCHAR(50), 
+    `nature` VARCHAR(50), 
+    PRIMARY KEY(`nom`)
+);
+
+INSERT INTO Plat (nom, nature)
+VALUES
+('salade_cesar', 'entrée');
+
+INSERT INTO Plat (nom, nature)
+VALUES
+('pizza', 'plat');
+
+INSERT INTO Plat (nom, nature)
+VALUES
+('bavette', 'plat');
+
+INSERT INTO Plat (nom, nature)
+VALUES
+('forêt_noire', 'dessert');
+
+INSERT INTO Plat (nom, nature)
+VALUES
+('crêpe', 'dessert');
+
+--15
+CREATE TABLE `Apprecie` ( 
+    `id` INT, 
+    `plat` VARCHAR(50), 
+    PRIMARY KEY(`id`,`plat`), 
+    FOREIGN KEY(`id`) REFERENCES Personne(`id`), 
+    FOREIGN KEY(`plat`) REFERENCES Plat(`nom`) 
+);
+
+CREATE TABLE `Deteste` ( 
+    `id` INT, 
+    `plat` VARCHAR(50), 
+    PRIMARY KEY(`id`,`plat`), 
+    FOREIGN KEY(`id`) REFERENCES Personne(`id`), 
+    FOREIGN KEY(`plat`) REFERENCES Plat(`nom`) 
+);
+
+INSERT INTO Apprecie (id, plat)
+VALUES
+(0, 'pizza');
+
+INSERT INTO Deteste (id, plat)
+VALUES
+(0, 'salade_cesar');
+
+INSERT INTO Apprecie (id, plat)
+VALUES
+(1, 'forêt_noire');
+
+INSERT INTO Deteste (id, plat)
+VALUES
+(1, 'bavette');
+
+INSERT INTO Apprecie (id, plat)
+VALUES
+(2, 'bavette');
+
+INSERT INTO Deteste (id, plat)
+VALUES
+(2, 'pizza');
+
+INSERT INTO Apprecie (id, plat)
+VALUES
+(3, 'salade_cesar');
+
+INSERT INTO Deteste (id, plat)
+VALUES
+(3, 'pizza');
+
+INSERT INTO Apprecie (id, plat)
+VALUES
+(4, 'pizza');
+
+INSERT INTO Deteste (id, plat)
+VALUES
+(4, 'bavette');
+
+INSERT INTO Apprecie (id, plat)
+VALUES
+(5, 'salade_cesar');
+
+INSERT INTO Deteste (id, plat)
+VALUES
+(5, 'forêt_noire');
+
+
+--16
+--a)
+SELECT * FROM Plat;
+--b)
+SELECT nom
+FROM (((Plat
+INNER JOIN Apprecie ON Apprecie.plat = Plat.nom)
+INNER JOIN Personne ON Personne.id = Apprecie.id)
+INNER JOIN Personne WHERE profession = 'Psychologue');
